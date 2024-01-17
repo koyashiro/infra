7d5e0fb948a55595d041cac48a5a69e360b86e21
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-01-17 22:35:13 +0900

    chore: migrate Terraform tfstate management from AWS S3 to Terraform Cloud

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,6 @@
 terraform {
   required_version = "1.6.6"
 
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "5.32.1"
-    }
-  }
-  backend "s3" {
-    encrypt = true
+  cloud {
   }
 }
