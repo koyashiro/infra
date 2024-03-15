4121b799e1542ee67e54476a2be18388ba528a97
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-03-16 02:11:22 +0900

    ci: add Terraform Cloudflare Provider (#109)

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,15 @@
 terraform {
   required_version = "1.7.5"
 
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "= 4.26.0"
+    }
+  }
+
   cloud {
   }
 }
+
+provider "cloudflare" {}
