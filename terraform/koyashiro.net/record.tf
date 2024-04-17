107eae14e42388cff670029c4f6feb19aa5717a5
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-04-18 00:01:55 +0900

    feat(terraform): update `koyashiro.net` dns records

--- terraform/koyashiro.net/record.tf
+++ terraform/koyashiro.net/record.tf
@@ -5,3 +5,86 @@ resource "cloudflare_record" "cname_root" {
   value   = "koyashi.ro"
   proxied = true
 }
+
+resource "cloudflare_record" "cname_vpm" {
+  zone_id = var.cloudflare_zone_id
+  type    = "CNAME"
+  name    = "vpm"
+  value   = "vpm-repos.pages.dev"
+  proxied = true
+}
+
+resource "cloudflare_record" "mx_primary" {
+  zone_id  = var.cloudflare_zone_id
+  type     = "MX"
+  name     = "@"
+  value    = "aspmx.l.google.com"
+  priority = 1
+}
+
+resource "cloudflare_record" "mx_alt1" {
+  zone_id  = var.cloudflare_zone_id
+  type     = "MX"
+  name     = "@"
+  value    = "alt1.aspmx.l.google.com"
+  priority = 5
+}
+
+resource "cloudflare_record" "mx_alt2" {
+  zone_id  = var.cloudflare_zone_id
+  type     = "MX"
+  name     = "@"
+  value    = "alt2.aspmx.l.google.com"
+  priority = 5
+}
+
+resource "cloudflare_record" "mx_alt3" {
+  zone_id  = var.cloudflare_zone_id
+  type     = "MX"
+  name     = "@"
+  value    = "alt3.aspmx.l.google.com"
+  priority = 10
+}
+
+resource "cloudflare_record" "mx_alt4" {
+  zone_id  = var.cloudflare_zone_id
+  type     = "MX"
+  name     = "@"
+  value    = "alt4.aspmx.l.google.com"
+  priority = 10
+}
+
+resource "cloudflare_record" "txt_spf" {
+  zone_id = var.cloudflare_zone_id
+  type    = "TXT"
+  name    = "@"
+  value   = "v=spf1 include:koyashiro.net ~all"
+}
+
+resource "cloudflare_record" "txt_google_suite_verification" {
+  zone_id = var.cloudflare_zone_id
+  type    = "TXT"
+  name    = "@"
+  value   = "google-site-verification=7-AYDWnsMkam_BpKeKWqjbeTqglhrp80t1N4RtKg6gQ"
+}
+
+resource "cloudflare_record" "txt_keybase_site_verification" {
+  zone_id = var.cloudflare_zone_id
+  type    = "TXT"
+  name    = "@"
+  value   = "keybase-site-verification=fFkMCr9xX06WBiCVODi28op8DCxmwfcYIN-iFSqEiwk"
+}
+
+resource "cloudflare_record" "txt_google_domainkey" {
+  zone_id = var.cloudflare_zone_id
+  type    = "TXT"
+  name    = "google._domainkey"
+  value   = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyhioqL5nv/wDnkrmguw+J1qpm2fsLev+PKZMVVCLuGurD/6UJUscAFRN5xpjbP4DOsTmQtP/pTj8M0+9DkRItkuJUGCb9U9DMi3MN2Ll9BAaEfKFUDg9OEudIW/kWBhawkQpKA1DREu/bnZPr8VvwcyrO+DhPRUHYsylEPWrCY8EdDSv4U/m4a2kc79l1aJLo3iC542LStRNw70ls8hKHWC8+49vup2FFWFn9ktSgQs21A5hDY+VB/vEnpqCyN8R+VP0q5QgmAxvwWtfGQ11kIE82+p6OOI2RNPHYt5wHlPtmyZCZh28l3PRrdzXXOJiV9KvC9FuAvJmHUuu9OoF5wIDAQAB"
+}
+
+resource "cloudflare_record" "txt_discord" {
+  zone_id = var.cloudflare_zone_id
+  type    = "TXT"
+  name    = "_discord"
+  value   = "dh=2f7777c55cca3b5519378571f05bd3979da45d38"
+}
