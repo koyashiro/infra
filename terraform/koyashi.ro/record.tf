d22cb84257b725330a63879b2ceb47b0841b422a
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-04-18 02:27:59 +0900

    refactor: use data `cloudflare_zone`

--- terraform/koyashi.ro/record.tf
+++ terraform/koyashi.ro/record.tf
@@ -1,5 +1,9 @@
+data "cloudflare_zone" "koyashi_ro" {
+  name = "koyashi.ro"
+}
+
 resource "cloudflare_record" "cname_root" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "CNAME"
   name    = "@"
   value   = "koyashiro.pages.dev"
@@ -7,7 +11,7 @@ resource "cloudflare_record" "cname_root" {
 }
 
 resource "cloudflare_record" "cname_blog" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "CNAME"
   name    = "blog"
   value   = "blog-koyashiro.pages.dev"
@@ -15,7 +19,7 @@ resource "cloudflare_record" "cname_blog" {
 }
 
 resource "cloudflare_record" "cname_cdn" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "CNAME"
   name    = "cdn"
   value   = "cdn-koyashiro.pages.dev"
@@ -23,7 +27,7 @@ resource "cloudflare_record" "cname_cdn" {
 }
 
 resource "cloudflare_record" "mx_primary" {
-  zone_id  = var.cloudflare_zone_id
+  zone_id  = data.cloudflare_zone.koyashi_ro.id
   type     = "MX"
   name     = "@"
   value    = "aspmx.l.google.com"
@@ -31,7 +35,7 @@ resource "cloudflare_record" "mx_primary" {
 }
 
 resource "cloudflare_record" "mx_alt1" {
-  zone_id  = var.cloudflare_zone_id
+  zone_id  = data.cloudflare_zone.koyashi_ro.id
   type     = "MX"
   name     = "@"
   value    = "alt1.aspmx.l.google.com"
@@ -39,7 +43,7 @@ resource "cloudflare_record" "mx_alt1" {
 }
 
 resource "cloudflare_record" "mx_alt2" {
-  zone_id  = var.cloudflare_zone_id
+  zone_id  = data.cloudflare_zone.koyashi_ro.id
   type     = "MX"
   name     = "@"
   value    = "alt2.aspmx.l.google.com"
@@ -47,7 +51,7 @@ resource "cloudflare_record" "mx_alt2" {
 }
 
 resource "cloudflare_record" "mx_alt3" {
-  zone_id  = var.cloudflare_zone_id
+  zone_id  = data.cloudflare_zone.koyashi_ro.id
   type     = "MX"
   name     = "@"
   value    = "alt3.aspmx.l.google.com"
@@ -55,7 +59,7 @@ resource "cloudflare_record" "mx_alt3" {
 }
 
 resource "cloudflare_record" "mx_alt4" {
-  zone_id  = var.cloudflare_zone_id
+  zone_id  = data.cloudflare_zone.koyashi_ro.id
   type     = "MX"
   name     = "@"
   value    = "alt4.aspmx.l.google.com"
@@ -63,35 +67,35 @@ resource "cloudflare_record" "mx_alt4" {
 }
 
 resource "cloudflare_record" "txt_spf" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "TXT"
   name    = "@"
   value   = "v=spf1 include:koyashi.ro ~all"
 }
 
 resource "cloudflare_record" "txt_google_suite_verification" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "TXT"
   name    = "@"
   value   = "google-site-verification=IXe9IiyOUytKAH9R0_KVE2Vmpt1iOB8kqwtSf-F4304"
 }
 
 resource "cloudflare_record" "txt_keybase_site_verification" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "TXT"
   name    = "@"
   value   = "keybase-site-verification=h6nNjeIuG_eU4fByG3qFMwbC8-sUeC5pvpD_m5iIUh4"
 }
 
 resource "cloudflare_record" "txt_google_domainkey" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "TXT"
   name    = "google._domainkey"
   value   = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAsN9es+6C1V6MGC8Scg/nHQOEd9rtNraw6hqX6gi24F3i8hAVtlDC2FrSukqUz8HH1WEdaGxPir2yvE8xD7OxIT4uOUinb7eXY5CaCRJL9lpjj82FBcVOkiXcuTEFHu04kkHSo3fuLCl8OHrTup8HUqPJ2VwUiCeOkqjoy7Hv9VZDQmwjPbRyVncjVb/sDtVeY2bVUix1acSo/g5mIrbcw2xWrxCU/VWcEfOvQypfE+z9UfBpbeAGWArBD+l3+vLAWFb3dsMc4CvRa3mGY8kehOMTHnaaf2gO5k7afAkSil/zK580WLHG0Hq6rEG9tsawydVbrkCqRRsV9yAWhyI02QIDAQAB"
 }
 
 resource "cloudflare_record" "txt_discord" {
-  zone_id = var.cloudflare_zone_id
+  zone_id = data.cloudflare_zone.koyashi_ro.id
   type    = "TXT"
   name    = "_discord"
   value   = "dh=f083205940112e59edc7561488d3867f414384a3"
--- terraform/koyashiro.net/variables.tf
+++ terraform/koyashiro.net/variables.tf
@@ -1,7 +1,3 @@
 variable "cloudflare_api_token" {
   type = string
 }
-
-variable "cloudflare_zone_id" {
-  type = string
-}
