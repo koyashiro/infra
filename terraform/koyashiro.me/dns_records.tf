3afaa3368c078ba1619a801f1b01ef313d6a49b4
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-04-23 03:25:06 +0900

    feat(terraform): add `koyashiro.me`

---
+++ terraform/koyashiro.me/dns_records.tf
@@ -0,0 +1,40 @@
+resource "cloudflare_record" "cname_root" {
+  zone_id = var.cloudflare_zone_id_koyashiro_me
+  type    = "CNAME"
+  name    = "@"
+  value   = "koyashi.ro"
+  proxied = true
+  ttl     = 1
+}
+
+resource "cloudflare_record" "txt_spf" {
+  zone_id = var.cloudflare_zone_id_koyashiro_me
+  type    = "TXT"
+  name    = "@"
+  value   = "v=spf1 -all"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "txt_dkim" {
+  zone_id = var.cloudflare_zone_id_koyashiro_me
+  type    = "TXT"
+  name    = "*._domainkey"
+  value   = "v=DKIM1; p="
+  ttl     = 1
+}
+
+resource "cloudflare_record" "txt_dmarc" {
+  zone_id = var.cloudflare_zone_id_koyashiro_me
+  type    = "TXT"
+  name    = "_dmarc"
+  value   = "v=DMARC1; p=reject; sp=reject; adkim=s; aspf=s;"
+  ttl     = 1
+}
+
+resource "cloudflare_record" "txt_keybase_site_verification" {
+  zone_id = var.cloudflare_zone_id_koyashiro_me
+  type    = "TXT"
+  name    = "@"
+  value   = "keybase-site-verification=mXW1Jxxi9Cxm_w4TvEv1g3eFJVpm95-i0VCWsxSmr8M"
+  ttl     = 1
+}
