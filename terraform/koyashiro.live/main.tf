550fd5f804644e1f5d02896c520de3eaa838d7b6
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-04-23 13:11:27 +0900

    feat(terraform): add vultr resource (#172)

--- terraform/koyashiro.live/main.tf
+++ terraform/koyashiro.live/main.tf
@@ -6,6 +6,11 @@ terraform {
       source  = "cloudflare/cloudflare"
       version = "4.30.0"
     }
+
+    vultr = {
+      source  = "vultr/vultr"
+      version = "2.19.0"
+    }
   }
 
   cloud {
@@ -19,4 +24,8 @@ terraform {
 
 provider "cloudflare" {
   api_token = var.cloudflare_api_token
+}
+
+provider "vultr" {
+  api_key = var.vultr_api_key
 }
