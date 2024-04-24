4e9bfc2a87ae5cb67e21964df599fad28f0b94f9
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-04-24 19:27:19 +0900

    feat(terraform): remove vultr ssh key resouce

--- terraform/koyashiro.live/ssh_keys.tf
+++
@@ -1,8 +0,0 @@
-data "http" "ssh_key" {
-  url = "https://github.com/koyashiro.keys"
-}
-
-resource "vultr_ssh_key" "ssh_key" {
-  name    = "koyashiro"
-  ssh_key = data.http.ssh_key.response_body
-}
