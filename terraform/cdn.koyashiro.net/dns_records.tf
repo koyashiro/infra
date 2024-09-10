cf4acac70eac2c57583d5f9ebbe411479605ab12
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-09-10 23:59:59 +0900

    feat(terraform): remove DNS records for `cdn.koyashiro.net`

--- terraform/cdn.koyashiro.net/dns_records.tf
+++
@@ -1,7 +0,0 @@
-resource "cloudflare_record" "cname_cdn" {
-  zone_id = var.cloudflare_zone_id_koyashiro_net
-  type    = "CNAME"
-  name    = "cdn"
-  content = var.r2_bucket_domain_cdn_koyashiro_net
-  proxied = true
-}
