bbb1315ec80488963004e325135c9b3a5cf7753b
koyashiro <6698252+koyashiro@users.noreply.github.com>
2024-10-17 20:14:14 +0900

    chore: manage Node.js with Volta instead of aqua

--- terraform/koyashiro.net/cloudflare_pages.tf
+++ terraform/koyashiro.net/cloudflare_pages.tf
@@ -25,12 +25,12 @@ resource "cloudflare_pages_project" "koyashiro_net" {
   deployment_configs {
     preview {
       environment_variables = {
-        NODE_VERSION = "22.8.0"
+        NODE_VERSION = "22.10.0"
       }
     }
     production {
       environment_variables = {
-        NODE_VERSION = "22.8.0"
+        NODE_VERSION = "22.10.0"
       }
     }
   }
