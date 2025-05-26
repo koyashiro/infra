87ecf23b3464206213dc820083af75ac6d9abb07
koyashiro <6698252+koyashiro@users.noreply.github.com>
2025-05-27 00:58:39 +0900

    feat(terraform): remove koyashiro.live resources

--- terraform/koyashiro.live/instances.tf
+++
@@ -1,8 +0,0 @@
-resource "vultr_instance" "instance" {
-  region      = "nrt" # Tokyo
-  plan        = "vhp-1c-1gb-intel"
-  os_id       = 1743 # Ubuntu 22.04 LTS x64
-  label       = "koyashiro.live"
-  hostname    = "koyashiro.live"
-  enable_ipv6 = true
-}
